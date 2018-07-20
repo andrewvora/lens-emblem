f23e4bd4efd6626b14956bb48be11e8864f602f2
Andrew Vorakrajangthiti <andrewjvora@gmail.com>
2018-07-19 22:59:20 -0700

    Revamped how the weapon modifiers worked.
    Added tests to prevent regressions.

--- scripts/weapon-modifiers.js
+++ scripts/weapon-modifiers.js
@@ -1,58 +1,64 @@
+'use strict'
 
+const weaponMods = {
+  // swords
+  'amiti': { stat: 'spd', mod: -2 },
+  'audhulma': { stat: 'res', mod: 5 },
+  'ayra\'s blade': { stat: 'spd', mod: 3 },
+  'beloved zofia': { stat: 'def', mod: 3 },
+  'blazing durandal': { stat: 'atk', mod: 3 },
+  'brave sword': { stat: 'spd', mod: -5 },
+  'divine tyrfing': { stat: 'res', mod: 3 },
+  'light brand': { stat: 'def', mod: 3 },
+  'meisterschwert': { stat: 'spd', mod: -5 },
+  'resolute blade': { stat: 'atk', mod: 3 },
+
+  // red tomes
+  'grima\'s truth': { stat: 'def', mod: 3 },
+  'loptous': { stat: 'res', mod: 3 },
+  'múspell fireposy': { stat: 'spd', mod: 3 },
+
+  // lance
+  'brave lance': { stat: 'spd', mod: -5 },
+  'flame siegmund': { stat: 'atk', mod: 3 },
+  'geirskögul': { stat: 'def', mod: 3 },
+
+  // blue tomes
+  'huginn\'s egg': { stat: 'res', mod: 3 },
+  'ivaldi': { stat: 'def', mod: 3 },
+  'thani': { stat: 'res', mod: 3 },
+  'weirding tome': { stat: 'spd', mod: 3 },
+
+  // axes
+  'brave axe': { stat: 'spd', mod: -5 },
+  'sinmara': { stat: 'def', mod: 3 },
+  'thunder armads': { stat: 'def', mod: 3 },
+  'blizzard': { stat: 'res', mod: 3 },
+  'giga excalibur': { stat: 'spd', mod: 3 },
+  'muninn\'s egg': { stat: 'res', mod: 3 },
+  'nifl frostflowers': { stat: 'atk', mod: 3 },
+
+  // breaths
+  'great flame': { stat: 'atk', mod: 3 },
+  'summer\'s breath': { stat: 'def', mod: 3 },
+
+  // bows
+  'brave bow': { stat: 'spd', mod: -5 },
+  'mulagir': { stat: 'spd', mod: 3 },
+  'skadi': { stat: 'spd', mod: 3 },
+  'swift mulagir': { stat: 'res', mod: 3 },
+  'warrior princess': { stat: 'spd', mod: 3 }
+}
+
+module.exports.weaponMods = weaponMods
 module.exports.apply = function(weapon, stats) {
-    const weaponName = weapon.name.toLowerCase()
+    const weaponName = weapon.name
+        .toLowerCase()
+        .trim()
+        .replace('^[a-zA-Z\'\s]', '')
 
-    if (weaponName.indexOf('brave') >= 0) {
-        stats['spd'] = stats['spd'] - 5
-    } else if (weaponName.indexOf('amiti') >= 0) {
-        stats['spd'] = stats['spd'] - 2
-    } else if (weaponName.indexOf('ayra') >= 0) {
-        stats['spd'] = stats['spd'] + 3
-    } else if (weaponName.indexOf('beloved zofia') >= 0) {
-        stats['def'] = stats['def'] + 3
-    } else if (weaponName.indexOf('blazing durandal') >= 0) {
-        stats['atk'] = stats['atk'] + 3
-    } else if (weaponName.indexOf('divine tyrfing') >= 0) {
-        stats['res'] = stats['res'] + 3
-    } else if (weaponName.indexOf('light brand') >= 0) {
-        stats['def'] = stats['def'] + 3
-    } else if (weaponName.indexOf('meisterschwert') >= 0) {
-        stats['spd'] = stats['spd'] - 5
-    } else if (weaponName.indexOf('resolute blade') >= 0) {
-        stats['atk'] = stats['atk'] + 3
-    } else if (weaponName.indexOf('gleipnir') >= 0) {
-        stats['res'] = stats['res'] + 3
-    } else if (weaponName.indexOf('grima') >= 0) {
-        stats['def'] = stats['def'] + 3
-    } else if (weaponName.indexOf('loptous') >= 0) {
-        stats['res'] = stats['res'] + 3
-    } else if (weaponName.indexOf('flame siegmund') >= 0) {
-        stats['atk'] = stats['atk'] + 3
-    } else if (weaponName.indexOf('geirsk') >= 0) {
-        stats['def'] = stats['def'] + 3
-    } else if (weaponName.indexOf('dire thunder') >= 0) {
-        stats['spd'] = stats['spd'] - 5
-    } else if (weaponName.indexOf('huginn') >= 0) {
-        stats['res'] = stats['res'] + 3
-    } else if (weaponName.indexOf('ivaldi') >= 0) {
-        stats['def'] = stats['def'] + 3
-    } else if (weaponName.indexOf('thani') >= 0) {
-        stats['res'] = stats['res'] + 3
-    } else if (weaponName.indexOf('weirding tome') >= 0) {
-        stats['def'] = stats['def'] + 3
-    } else if (weaponName.indexOf('blizzard') >= 0) {
-        stats['res'] = stats['res'] + 3
-    } else if (weaponName.indexOf('muninn') >= 0) {
-        stats['res'] = stats['res'] + 3
-    } else if (weaponName.indexOf('great flame') >= 0) {
-        stats['atk'] = stats['atk'] + 3
-    } else if (weaponName.indexOf('skadi') >= 0) {
-        stats['spd'] = stats['spd'] + 3
-    } else if (weaponName.indexOf('swift mulagir') >= 0) {
-        stats['res'] = stats['res'] + 3
-    } else if (weaponName.indexOf('warrior princess') >= 0) {
-        stats['spd'] = stats['spd'] + 3
-    } else if (weaponName.indexOf('fireposy') >= 0) {
-        stats['spd'] = stats['spd'] + 3
+    const match = weaponMods[weaponName]
+    if (match != undefined) {
+        stats[match.stat] = stats[match.stat] + match.mod
     }
 }
