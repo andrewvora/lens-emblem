d01cdb750166f4b1532b2a9fcb87758ed3920225
Andrew Vorakrajangthiti <andrewjvora@gmail.com>
2018-12-28 00:09:33 -0800

    Updated weapon modifiers.

--- scripts/weapon-modifiers.js
+++ scripts/weapon-modifiers.js
@@ -9,17 +9,29 @@ const weaponMods = {
   'blazing durandal': { stat: 'atk', mod: 3 },
   'brave sword': { stat: 'spd', mod: -5 },
   'divine tyrfing': { stat: 'res', mod: 3 },
+  'exalted falchion': { stat: 'spd', mod: 3 },
   'light brand': { stat: 'def', mod: 3 },
   'meisterschwert': { stat: 'spd', mod: -5 },
+  'gjöll': { stat: 'atk', mod: 3 },
   'resolute blade': { stat: 'atk', mod: 3 },
   'royal sword': { stat: 'spd', mod: 3 },
   'laevatein': { stat: 'atk', mod: 3 },
+  'light brand': { stat: 'def', mod: 3 },
   'exalted falchion': { stat: 'spd', mod: 3 },
   'níu': { stat: 'spd', mod: 3 },
   'niu': { stat: 'spd', mod: 3 },
+  'resolute blade': { stat: 'atk', mod: 3 },
+  'royal sword': { stat: 'atk', mod: 3 },
+  'storm sieglinde': { stat: 'atk', mod: 3 },
 
   // red tomes
+  'aversa\'s night' : { stat: 'res', mod: 3 },
+  'book of dreams': { stat: 'atk', mod: 3 },
+  'fruit of iðunn': { stat: 'spd', mod: 3 },
+  'fruit of iounn': { stat: 'spd', mod: 3 },
+  'gleipnir': { stat: 'res', mod: 3 },
   'grima\'s truth': { stat: 'def', mod: 3 },
+  'imhullu': { stat: 'res', mod: 3 },
   'loptous': { stat: 'res', mod: 3 },
   'múspell fireposy': { stat: 'spd', mod: 3 },
 
@@ -29,24 +41,33 @@ const weaponMods = {
   'geirskögul': { stat: 'def', mod: 3 },
 
   // blue tomes
+  'dire thunder': { stat: 'spd', mod: -5 },
   'huginn\'s egg': { stat: 'res', mod: 3 },
   'ivaldi': { stat: 'def', mod: 3 },
+  'prayer wheel': { stat: 'spd', mod: 3 },
+  'sagittae': { stat: 'def', mod: 3 },
   'thani': { stat: 'res', mod: 3 },
   'weirding tome': { stat: 'spd', mod: 3 },
 
   // axes
   'brave axe': { stat: 'spd', mod: -5 },
+  'cherche\'s axe': { stat: 'spd', mod: -5 },
+  'garm': { stat: 'atk', mod: 3 },
   'sinmara': { stat: 'def', mod: 3 },
   'thunder armads': { stat: 'def', mod: 3 },
+  'wolf berg': { stat: 'def', mod: 3 },
+  
+  // green tomes
   'blizzard': { stat: 'res', mod: 3 },
+  'book of shadows': { stat: 'spd', mod: 3 },
+  'forseti': { stat: 'spd', mod: 3 },
   'giga excalibur': { stat: 'spd', mod: 3 },
   'muninn\'s egg': { stat: 'res', mod: 3 },
   'nifl frostflowers': { stat: 'atk', mod: 3 },
-  'wolf berg': { stat: 'def', mod: 3 },
-  'garm': { stat: 'atk', mod: 3 },
 
   // breaths
   'great flame': { stat: 'atk', mod: 3 },
+  'spirit breath': { stat: 'def', mod: 3 },
   'summer\'s breath': { stat: 'def', mod: 3 },
 
   // bows
@@ -54,11 +75,13 @@ const weaponMods = {
   'mulagir': { stat: 'spd', mod: 3 },
   'skadi': { stat: 'spd', mod: 3 },
   'swift mulagir': { stat: 'res', mod: 3 },
-  'warrior princess': { stat: 'spd', mod: 3 },
   'thögn': { stat: 'spd', mod: 3 },
+  'warrior princess': { stat: 'spd', mod: 3 },
 
   // daggers
-  'dawn suzu': { stat: 'atk', mod: 3 }
+  'dawn suzu': { stat: 'atk', mod: 3 },
+  'lyfjaberg': { stat: 'res', mod: 3 },
+  'sylgr': { stat: 'spd', mod: 3 }
 }
 
 module.exports.weaponMods = weaponMods
